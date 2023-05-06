51e572e566c9ce70ee46d890244b20933a0d7d0b
jromero093 <127344540+jromero093@users.noreply.github.com>
2023-05-06 16:27:17 -0400

    Update car_track_OOP.py
    Have continued work on Mustang Class

--- car_track_OOP.py
+++ car_track_OOP.py
@@ -1,5 +1,11 @@
 from time import sleep
 
+# Import Console for console text printing
+from rich.console import Console
+
+# Initialize rich.console
+console = Console()
+
 # TODO Create class
 class Car():
     # Use __init__ method to initialize the
@@ -16,39 +22,51 @@ class Car():
 
 class Mustang:
     # Use __init__ method to initialize the
-    # two attributes as parameters
+    # three attributes as parameters
     def __init__(self):
             self._speed = 0
-            self._nos_use = "y"    # To keep track of one shot of NOS
+            self._nos_use = "y"     # To keep track of one shot of NOS
+            self._take_off = "y"    # Too keep track of whether you've already taken off
 
-    # Method for testing
+    # Introduce car
     def car_name(self):
-            print("You've chosen the 2012 Mustang Boss 302 Laguna Seca edition.")
-            sleep(3)
+            console.print("You've chosen the [bold blue]2012 Mustang Boss 302: Laguna Seca Edition[/bold blue].")
+            #sleep(3)
             print("You're gonna have fun.\n")
-            sleep(1)
+            #sleep(1)
 
-    # Define class methods
     def take_off(self):
-        # Use sleep to imitate take 0ff
-        sleep(.5)
-        print("20 mph")
-        sleep(1)
-        print("40 mph")
-        sleep(1)
-        print("60 mph!")
+        # Check if car has taken off already
+        if self._take_off == "x":
+            print("The car's already moving. Duh")
+            print(f"Speed remains {self._speed}")        
+        elif self._take_off == "y":
+            sleep(.5)
+            print("20 mph")
+            sleep(.5)
+            print("40 mph")
+            sleep(.5)
+            print("60 mph!")
+            self._take_off = "x"
 
-        self._speed += 60
-        sleep(.5)
-        print(f"Current speed is {self._speed}")
+            self._speed += 60
+            sleep(.5)
+            print(f"Current speed is {self._speed}")
     
-    def accelerate(self):       
+    def accelerate(self):
             self._speed += 20
             print(f"Current speed is {self._speed}")
+
+            # Check speed
+            self.top_speed_situation()
 
     def decelerate(self):
             self._speed -= 20
             print(f"Current speed is {self._speed}")
+            if self._speed < 60:
+                # Encourage them to step! on! it!
+                sleep(.5)
+                print("\n(You know, you can go a little faster if you want.)")
 
     def nos(self):
         # Use while loop since only one NOS shot may be used
@@ -65,6 +83,10 @@ class Mustang:
             print("NOS used!")
             print(f"Current speed is {self._speed}")
             self._nos_use = "x"
+
+            # Check speed
+            self.top_speed_situation()
+            
         
 
     def pull_over(self):
@@ -78,7 +100,28 @@ class Mustang:
         print("Pulled over.")
         print(f"Current speed is {self._speed}")
 
-          
+    def top_speed_situation(self):
+            # if statements to check for top speed situation
+            if self._speed >= 165:
+                sleep(1)
+                console.print("[red]Warning. Warning.[/red] Engine overheat.")
+                print("Engine imploding.")
+                sleep(2.5)
+                for i in range (3):
+                    print("...")
+                    sleep(1)
+                print("*Crash crash bang bang*")
+                sleep(3)
+                print("You've crashed. A total wreck.")
+                sleep(3)
+                print("But you'll recover. And you'll drive again.")
+                sleep(2.5)
+                print("It's what you do.")
+                sleep(2.5)
+                quit()
+            elif self._speed >= 140:
+                sleep(1)
+                console.print("[red]Warning[/red]: Top Speed Reached\n[red]Do no accelerate[/red] (Unless you can't help it)")
     
 
 
@@ -105,23 +148,23 @@ class Corvette:
 
 
 
-# ------------------------------------------- MAIN --------------------------------------------- #
+# ---------------------------------------------- MAIN ------------------------------------------------ #
 def main():
     # Create object from Car classs
-    car = Car()
-    car.car_name()
+    #car = Car()
+    #car.car_name()
 
     # Ask for driver name
     driver_name = input("What's your name, driver: ")
 
     # Print car inventory and specs
     print(f"\n{driver_name}, here are your car choices:")
-    print("1 - 2012 Mustang Boss 302: Laguna Seca Edition")
+    console.print("1 - [bold blue]2012 Mustang Boss 302: Laguna Seca Edition[/bold blue]")
     print("         Horse power: 444    0-60 in: 4.6 secs    Top Speed: 145 mph ")  # Actually 146 mph
-    print("2 - 2011 Nissan GTR - aka The Supercar Killer")
-    print("         Horse power: 485    0-60 in: 3.5 secs    Top Speed: ?? mph ")
-    print("3 - Corvette ZR1 ")
-    print("         Horse power: ??     0-60 in: ??    Top Speed: ?? mph ")
+    console.print("2 - [bold blue]2011 Nissan GTR - aka 'The Supercar Killer'[/bold blue]")
+    print("         Horse power: 485    0-60 in: 3.3 secs    Top Speed: 190 mph ")  # Actually 193 mph
+    console.print("3 - [bold blue]2011 Corvette Z06 - The Led Zepellin of Performance Cars[/bold blue]")
+    print("         Horse power: 505     0-60 in: 3.8 secs    Top Speed: ?? mph ")
 
     # Ask for user car choice
     print('')
